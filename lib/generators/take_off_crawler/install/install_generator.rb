61c3a07bc1a3cebea29cbbb47130973ec7be5928
Flavia Grosan <me@flaviagrosan.com>
2012-02-12 16:15:53 +0100

    model generator

--- lib/generators/take_off_crawler/install/install_generator.rb
+++ lib/generators/take_off_crawler/install/install_generator.rb
@@ -4,8 +4,10 @@ module TakeOffCrawler
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
       include Rails::Generators::Migration
+      include Rails::Generators::Model
+      
       source_root File.expand_path('../templates', __FILE__)
-      desc "add the migration for Link model"
+      desc "add the migration and model for Link"
 
       def self.next_migration_number(path)
         unless @prev_migration_nr
@@ -16,8 +18,12 @@ module TakeOffCrawler
         @prev_migration_nr.to_s
       end
 
-      def copy_migrations
+      def copy_migration
         migration_template "create_links.rb", "db/migrate/create_links.rb"
+      end
+      
+      def copy_model
+        template "link.rb", "app/models/link.rb"
       end
     end
   end
--- lib/generators/take_off_crawler/install/templates/create_links.rb
+++ lib/generators/take_off_crawler/install/templates/create_links.rb
@@ -3,7 +3,9 @@ class CreateLinks < ActiveRecord::Migration
     create_table :links do |t|
       t.text :images
       t.string :link
-
+      t.text :meta_content
+      t.text :title
+      t.string :youtube_id
       t.timestamps
     end
   end
